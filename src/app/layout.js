cbfb04ce45ab9c449dc8baf6bc07d5df26bf381b
Perwez Alam <parwez4568@gmail.com>
2024-11-22 18:11:03 +0530

    add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "@/component/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        {/* footer */}
+        <Footer/>
       </body>
     </html>
   );
--- src/component/HomeSearch.jsx
+++ src/component/HomeSearch.jsx
@@ -5,26 +5,25 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 
 export const HomeSearch = () => {
-  const router = useRouter();  
+  const router = useRouter();
   const [input, setInput] = useState("");
   const [randomSearchLoading, setRandomSearchLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!input.trim())
-       return;
+    if (!input.trim()) return;
     router.push(`/search/web?searchTerm=${input}`);
   };
 
-  const randomSearch = async () =>{
+  const randomSearch = async () => {
     setRandomSearchLoading(true);
-    const response = await fetch('https://random-word-api.herokuapp.com/word');
+    const response = await fetch("https://random-word-api.herokuapp.com/word");
     const data = await response.json();
     const result = data[0];
-    if(!result) return;
-    router.push(`/search/web?searchTerm=${result}`)
+    if (!result) return;
+    router.push(`/search/web?searchTerm=${result}`);
     setRandomSearchLoading(false);
-  }
+  };
   return (
     <>
       <form
@@ -41,11 +40,22 @@ export const HomeSearch = () => {
         <BsFillMicFill className="text-lg" />
       </form>
       <div className="flex md:flex-row flex-col justify-center gap-4 mt-4">
-        <button onClick={handleSubmit} className="btn">Google Search</button>
-        <button onClick={randomSearch} className="btn flex items-center justify-center">
+        <button onClick={handleSubmit} className="btn">
+          Google Search
+        </button>
+        <button
+          onClick={randomSearch}
+          className="btn flex items-center justify-center"
+        >
           {randomSearchLoading ? (
-            <img src="spinner.svg" alt="loading..." className="h-6 text-center"/>
-          ) : ("I Am Feeling Lucky")}
+            <img
+              src="spinner.svg"
+              alt="loading..."
+              className="h-6 text-center"
+            />
+          ) : (
+            "I Am Feeling Lucky"
+          )}
         </button>
       </div>
     </>
