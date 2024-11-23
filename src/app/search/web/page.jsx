9f956c6e714df45c121ca0cde257f134d8eced60
Perwez Alam <parwez4568@gmail.com>
2024-11-23 15:51:23 +0530

    fetch data from google search api and shows the title

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,31 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const WebSearchPage = () => {
-  return (
-    <div>
-      WebSearchPage
-    </div>
-  )
-}
+  const [results,setResults] = useState(null);
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get('searchTerm');
 
-export default WebSearchPage
+  const handleGoogleApi = async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchTerm}`
+    );
+    const data = await response.json();
+    setResults(data.items);
+  };
+
+  useEffect(() => {
+    handleGoogleApi();
+  }, [searchTerm]);
+
+  return (
+    <>
+     {results && results.map((result) =>{
+         return <h1 key={result}>{result.title}</h1>
+     })}
+  </>
+  )
+};
+
+export default WebSearchPage;
