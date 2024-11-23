d7e5d39ddcd1564f9edf7cea44ba52bd61ae0e97
Perwez Alam <parwez4568@gmail.com>
2024-11-23 12:55:16 +0530

    complete the search box component

--- src/component/SearchBox.jsx
+++ src/component/SearchBox.jsx
@@ -1,10 +1,35 @@
+"use client";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
 
 const SearchBox = () => {
-  return (
-    <div>
-      <input type="text" />
-    </div>
-  )
-}
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
 
-export default SearchBox
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl"
+    >
+      <input
+        type="search"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+        className="w-full focus:outline-none px-2"
+      />
+      <BsFillMicFill className="hidden sm:flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+      <AiOutlineSearch onClick={handleSubmit} className="text-2xl hidden sm:flex text-blue-500 cursor-pointer" />
+    </form>
+  );
+};
+
+export default SearchBox;
