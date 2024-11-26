bdd218fb9797f415d7d0bc6a0ed005d4984183f7
Perwez Alam <parwez4568@gmail.com>
2024-11-26 16:09:27 +0530

    uninstall html-parser

--- src/component/WebSearchResults.jsx
+++ src/component/WebSearchResults.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Parser from "html-react-parser"
 import PaginationButtons from "./PaginationButtons"
 
 const WebSearchResults = ({results}) => {
@@ -16,7 +15,7 @@ const WebSearchResults = ({results}) => {
                     <Link className="text-sm truncate" href={result.link}>{result.formattedUrl}</Link>
                     <Link className="group-hover:underline decoration-blue-800 text-xl truncate font-medium text-blue-800" href={result.link}>{result.title}</Link>
                 </div>
-                <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+                <p className="text-gray-600">{result.htmlSnippet}</p>
             </div>
         })
        }
