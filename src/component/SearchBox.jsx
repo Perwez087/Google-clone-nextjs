d6a517a59baf81d0cbb4bb0f0a4dd0390406faed
Perwez Alam <parwez4568@gmail.com>
2024-11-26 16:44:18 +0530

    add suspense

--- src/component/SearchBox.jsx
+++ src/component/SearchBox.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 
@@ -17,19 +17,24 @@ const SearchBox = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl"
-    >
-      <input
-        type="search"
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
-        className="w-full focus:outline-none px-2"
-      />
-      <BsFillMicFill className="hidden sm:flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
-      <AiOutlineSearch onClick={handleSubmit} className="text-2xl hidden sm:flex text-blue-500 cursor-pointer" />
-    </form>
+    <Suspense>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl"
+      >
+        <input
+          type="search"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          className="w-full focus:outline-none px-2"
+        />
+        <BsFillMicFill className="hidden sm:flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+        <AiOutlineSearch
+          onClick={handleSubmit}
+          className="text-2xl hidden sm:flex text-blue-500 cursor-pointer"
+        />
+      </form>
+    </Suspense>
   );
 };
 
